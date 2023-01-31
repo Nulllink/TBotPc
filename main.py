a7435b5d9d766b267ff66a4b590c1e2d245f6545
Nulllink <47364651+Nulllink@users.noreply.github.com>
2023-01-31 19:32:49 +0300

    buttons

--- main.py
+++ main.py
@@ -1,41 +1,66 @@
+import re
+import time
+import keyboard
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.exceptions import BotBlocked
+
 f = open("token.txt", "r")
 TOKEN = f.readline()
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-import telebot
-bot = telebot.TeleBot(TOKEN)
-import keyboard
-
-mode = 'key'
+mode = 'buttons'
 last_msg = 'emp'
+b_mode = False
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+@dp.message_handler(content_types=['text'])
+async def get_text_messages(message: types.Message):
     global mode
     global last_msg
+    global b_mode
     m: str = message.text
     m = m.lower()
 
-    if m == 'mode':
-        bot.send_message(message.from_user.id, f"Change mode on: key, write")
-    elif last_msg == 'mode':
-        mode = m
-        bot.send_message(message.from_user.id, f"I changed mode to: {m}")
-    else:
-        if mode == 'key':
-            blocks = m.split(' ')
-            if len(blocks) > 1:
-                times = int(blocks[1])
-                while times > 0:
-                    times -= 1
-                    keyboard.send(blocks[0])
-                bot.send_message(message.from_user.id, f"I pressed: {m} times")
-            else:
-                keyboard.send(m)
-                bot.send_message(message.from_user.id, f"I pressed: {m}")
-        elif mode == 'write':
-            keyboard.write(m, 0.1)
-            bot.send_message(message.from_user.id, f"I wrote: {m}")
+    rus = re.search('[а-яА-Я]', m)
+    if rus is None:
+        if m == 'mode':
+            board = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            buttons = ["key", "write", "buttons"]
+            board.add(*buttons)
+            await message.answer(f"Change mode on", reply_markup=board)
+        elif last_msg == 'mode':
+            mode = m
+            await message.reply(f"I changed mode to: {m}")
+            print(f"I changed mode to: {m}")
+        else:
+            if mode == 'key' or b_mode:
+                blocks = m.split(' ')
+                if len(blocks) > 1:
+                    times = int(blocks[1])
+                    while times > 0:
+                        times -= 1
+                        keyboard.send(blocks[0])
+                        time.sleep(0.5)
+                    await message.reply(f"I pressed: {m} times")
+                    print(f"I pressed: {m} times")
+                else:
+                    keyboard.send(m)
+                    await message.reply(f"I pressed: {m}")
+                    print(f"I pressed: {m}")
+                b_mode = False
+            elif mode == 'write':
+                keyboard.write(m, 0.5)
+                await message.reply(f"I wrote: {m}")
+                print(f"I wrote: {m}")
+            if mode == 'buttons':
+                board = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                buttons = ["space", "backspace", "delete"]
+                board.row(*buttons)
+                buttons = ["left", "right", "up", "down"]
+                board.row(*buttons)
+                await message.answer("Press button", reply_markup=board)
+                b_mode = True
     last_msg = m
 
 print('Bot started')
-bot.polling(none_stop=True, interval=0)
+executor.start_polling(dp,skip_updates=True)
